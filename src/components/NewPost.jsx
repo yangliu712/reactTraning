6968fe27ff4080be223b40bb6aa6badeb4b1bed8
MrYangsS <67302078+MrYangsS@users.noreply.github.com>
2024-05-16 15:20:38 -0500

    5/16 2

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,25 +1,47 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react";
+function NewPost({ onCancel, onSubmit }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-function NewPost({ authorChange, textareaChange, onCancel }) {
+  function textareaChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    onSubmit(postData);
+    console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea
           id="body"
           required
           rows={3}
-          onChange={textareaChange}
+          onChange={textareaChangeHandler}
         ></textarea>
       </p>
 
       <p>
         <label htmlFor="name">your name</label>
-        <input type="text" id="name" required onChange={authorChange} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
 
-      <p>
-        <button>Submit</button>
+      <p className={classes.actions}>
+        <button> Submit</button>
         <button type="button" onClick={onCancel}>
           Cancel
         </button>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
 function PostList({ isPosting, onClose }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  function textareaChangeHandler(event) {
-    setEnteredBody(event.target.value);
+  useEffect(() => {
+    async function fectchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fectchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "post",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((old) => [postData, ...old]);
   }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onClose}>
-          <NewPost
-            textareaChange={textareaChangeHandler}
-            authorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onClose} onSubmit={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="heiheihei" body="asflasjflkjsdlfajslasdfasdfa" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>this is no post</h2>
+          <p>aaa ?</p>
+        </div>
+      )}
     </>
   );
 }
